0e191df1d9c873840ae5f16276ca451acd871950
JoelColucci <JoelColucci@Gmail.com>
2015-06-27 15:35:54 -0400

    feat: add review module

---
+++ review.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+"""Module contains functions that provide high level overview of
+OSM XML data.
+"""
+
+
+import xml.etree.cElementTree as ET
+
+
+def count_tags(filename):
+    """Return dict containing name and count of top level tags"""
+    tags = {}
+
+    for _, elem in ET.iterparse(filename):
+        if elem.tag in tags:
+            tags[elem.tag] += 1
+        else:
+            tags[elem.tag] = 1
+
+    return tags
+
+
+def get_number_of_contributors(filename):
+    """Return number of unique map contributors."""
+    users = set()
+
+    for _, element in ET.iterparse(filename):
+        uid = element.get("uid")
+        if uid:
+            users.add(uid)
+
+    return len(users)
+
+
+if __name__ == '__main__':
+    OSM_FILE = 'somerville-xml.osm'
+    print count_tags(OSM_FILE)
