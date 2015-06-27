bc57ebe3a64239667e80297dde82a150622df069
JoelColucci <JoelColucci@Gmail.com>
2015-06-27 15:20:13 -0400

    refactor: improve tagauditor module logic

--- tagauditor.py
+++ tagauditor.py
@@ -3,6 +3,7 @@
 """Module contains functions that analyze OSM XML elements of type tag.
 """
 
+
 import re
 import xml.etree.cElementTree as ET
 
@@ -38,6 +39,90 @@ def _validate_key_type(keys, k_val):
     return keys
 
 
+def get_k_value_summary(filename):
+    """Count all k values and number of variations of each."""
+    data = count_elem_attr_vals(filename, "tag", "k")
+    summary_data = {}
+
+    for key in data:
+        key_obj = data[key]
+        count, variations = _update_key_count(key_obj)
+
+        summary_data[key] = {"count": count, "variations": variations}
+
+    return summary_data
+
+
+def get_k_value_breakdown(filename):
+    """Break down and count all key values, single and compound. Represent
+    as nested dictionary data structure.
+    """
+    return count_elem_attr_vals(filename, "tag", "k")
+
+
+def count_elem_attr_vals(filename, elem_name, attr_name):
+    """Return dictionary; keys = tag key names, values = count of key """
+    keys = {}
+
+    for _, elem in ET.iterparse(filename):
+        if elem.tag == elem_name:
+            attr = elem.get(attr_name)
+            keys = _add_key(keys, attr)
+
+    return keys
+
+
+def _update_key_count(obj, count=0, variations=0):
+    """Recursively find and update count of key."""
+    for key in obj:
+        # Base Case
+        if key == "root":
+            count += obj[key]
+            variations += 1
+            continue
+
+        new_obj = obj[key]
+        count, variations = _update_key_count(new_obj, count, variations)
+
+    return count, variations
+
+
+def _add_key(keys, tag):
+    """"Recursively parse key and add to keys dict."""
+    # Recursive base case
+    if len(_split_keys(tag)) == 1:
+        if tag in keys:
+            if "root" in keys[tag]:
+                keys[tag]["root"] += 1
+        else:
+            keys[tag] = {"root": 1}
+
+        return keys
+
+    cur_key = _split_keys(tag)[0]
+    next_key = _split_keys(tag)[1]
+
+    # Get key value
+    cur_obj = keys.get(cur_key)
+
+    if cur_obj:
+        next_obj = cur_obj
+    else:
+        next_obj = {}
+
+    # Recursive call
+    updated_keys = _add_key(next_obj, next_key)
+
+    if updated_keys:
+        keys[cur_key] = updated_keys
+        return keys
+
+
+def _split_keys(key):
+    """Returns list; splits string on colon"""
+    return key.split(":", 1)
+
+
 if __name__ == '__main__':
     OSM_FILE = 'somerville-xml.osm'
-    print validate_k_attributes(OSM_FILE)
+    print get_k_value_breakdown(OSM_FILE)
